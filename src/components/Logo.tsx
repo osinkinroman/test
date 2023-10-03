e0741eac5491f313f376470734b09f3f685ae644
osinkinroman <osinkinroman@gmail.com>
2023-10-03 18:17:04 +0400

    Add basic layout for Navbar.tsx

---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+interface Props {}
+
+export const Logo = (props: Props) => {
+  return <div className="flex">Logo</div>;
+};
---
+++ src/components/profile/Profile.tsx
@@ -0,0 +1,10 @@
+interface Props {}
+
+export const Profile = (props: Props) => {
+  return (
+    <div className="flex">
+      <img></img>
+      <div>Profile name</div>
+    </div>
+  );
+};
